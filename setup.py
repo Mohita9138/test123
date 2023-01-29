93d7a0f94eff76278f053b3e528ded4a70626296
Mr. Robot <90469527+Mohita9138@users.noreply.github.com>
2023-01-29 23:45:56 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import os
+import subprocess
+import banner
+
+def main():
+    os.system("clear")
+    banner.main()
+    banner.attack("Setup")
+    os.system("pip install -r requirements.txt")
+    create_symlink()
+def create_symlink():
+    script_path = os.path.abspath("cyberonix.py")
+    link_path = os.path.join("/usr/local/bin", "cyberonix")
+    subprocess.run(["ln", "-s", script_path, link_path])
+    finish()
+def finish():
+    os.system("clear")
+    banner.main()
+    banner.attack("Setup Completed")
+    os.system("cyberonix")
+
+if __name__ == "__main__":
+    main()
+
