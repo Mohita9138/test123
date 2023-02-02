ec0696d521decdd8532c760db03d170634730d0d
Mohita9138 <mohit66.mg@gmail.com>
2023-02-02 22:03:22 +0530

    hardik

--- main/tools/information_gathering.py
+++ main/tools/information_gathering.py
@@ -271,7 +271,7 @@ def Netdiscover():
         if ask=="1":
                 print(f"{colors.blue}[+] Download/usage")
                 print(f"\nPreinstalled in Repository{colors.reset}")
-                check_installed("netdiscover","no-help")
+                check_installed("netdiscover","-h")
                 waiting.waiting()
         elif ask=="2": 
             writeup.writeup({"First resource":"https://subscription.packtpub.com/book/networking-and-servers/9781789341768/5/ch05lvl1sec50/scanning-with-netdiscover"},"Netdiscover")
