c68a140348461715b80581f2a77b9f72a035b699
Mohita9138 <mohit66.mg@gmail.com>
2023-02-03 00:41:50 +0530

    hardik

--- main/tools/WEB_Application_Analysis.py
+++ main/tools/WEB_Application_Analysis.py
@@ -28,7 +28,7 @@ try:
                                 threading.Thread(target=thread_run, args=(name,)).start()
         else:
                     print(f"{colors.green}[+] Installed")
-                    print(f"[+] it is installed in your kali"{colors.reset})
+                    print(f"[+] it is installed in your kali{colors.reset}")
                     if needargs:
                             download=input(f"{colors.blue}Do you want to run the tool?(y/n):{colors.reset}")
                             if download=="y" or download=="Y" or download=="Yes" or download=="yes":
