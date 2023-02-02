c2d4984e73b019a7f7756b3d247524632d7d091c
Mohita9138 <mohit66.mg@gmail.com>
2023-02-02 21:29:43 +0530

    hardik

--- main/tools/information_gathering.py
+++ main/tools/information_gathering.py
@@ -29,8 +29,8 @@ def check_installed(name,needargs=False):
                             #when tool is of gui it needs thread
                             threading.Thread(target=thread_run, args=(name,)).start()
     else:
-                print(f"{color.green}[+] Installed")
-                print(f"[+] it is installed in your kali{color.reset}")
+                print(f"{colors.green}[+] Installed")
+                print(f"[+] it is installed in your kali{colors.reset}")
                 if needargs:
                         download=input(f"{colors.blue}Do you want to run the tool?(y/n){colors.reset}")
                         if download=="y" or download=="Y" or download=="Yes" or download=="yes":
@@ -147,20 +147,20 @@ def Dracnmap():
         ask = tool_options()
         if ask == "1":
             print(f"{colors.blue}[+] Download/usage")
-            print(f"{color.blue}Checking Dracnmap is installed or not......{color.reset}")
+            print(f"{colors.blue}Checking Dracnmap is installed or not......{colors.reset}")
             if not os.path.isdir("Dracnmap"):
-                print(f"{color.red}[-] Dracnmap is not installed{color.reset}")
-                check_y = input(f"[{color.blue}+] Do you want to install it?(y/n){color.reset}")
+                print(f"{colors.red}[-] Dracnmap is not installed{colors.reset}")
+                check_y = input(f"[{colors.blue}+] Do you want to install it?(y/n){colors.reset}")
                 if check_y == "y" or check_y== "Y" or check_y == "yes" or check_y=="YES":
-                    print(f"{color.green}[+] Installing .......")
+                    print(f"{colors.green}[+] Installing .......")
                     os.system("git clone https://github.com/Screetsec/Dracnmap.git")
-                    print(f"Dracnmap is installed at ' {os.getcwd()} ' path\n{color.reset}")
-                    use = input(f"{color.blue}[+] Do you want to start Dracnmap? (y/n){color.reset}")
+                    print(f"Dracnmap is installed at ' {os.getcwd()} ' path\n{colors.reset}")
+                    use = input(f"{colors.blue}[+] Do you want to start Dracnmap? (y/n){colors.reset}")
                     if use[0].lower() == "y":
                         os.system("cd Dracnmap && chmod +x dracnmap-v2.2.sh && sudo ./dracnmap-v2.2.sh.sh")
             else:
-                print(f"{color.green}[+] Dracnmap is already Installed !!")
-                use = input(f"{color.blue}[+] Do you want to start Dracnmap? (y/n){color.reset}")
+                print(f"{colors.green}[+] Dracnmap is already Installed !!")
+                use = input(f"{colors.blue}[+] Do you want to start Dracnmap? (y/n){colors.reset}")
                 if use[0].lower() == "y":
                     	os.system("cd Dracnmap && chmod +x dracnmap-v2.2.sh && ./dracnmap-v2.2.sh.sh")
         elif ask == "2":
@@ -176,20 +176,20 @@ def RED_HAWK():
         ask = tool_options()
         if ask == "1":
             print(f"{colors.blue}[+] Download/usage")
-            print(f"{color.blue}Checking RED_HAWK is installed or not......{color.reset}")
+            print(f"{colors.blue}Checking RED_HAWK is installed or not......{colors.reset}")
             if not os.path.isdir("RED_HAWK"):
-                print(f"{color.red}[-] RED_HAWK is not installed{color.reset}")
-                check_y = input(f"{color.blue}[+] Do you want to install it?(y/n){color.reset}")
+                print(f"{colors.red}[-] RED_HAWK is not installed{colors.reset}")
+                check_y = input(f"{colors.blue}[+] Do you want to install it?(y/n){colors.reset}")
                 if check_y.lower() == "y" or check_y.lower()=="YES":
-                    print(f"{color.green}[+] Installing .......{color.reset}")
+                    print(f"{colors.green}[+] Installing .......{colors.reset}")
                     os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK.git")
-                    print(f"{color.green}\RED_HAWK is installed at ' {os.getcwd()} ' path\n{color.reset}")
-                    ask_s = input(f"{color.blue}[+] Do you want to start RED_HAWK? (y/n){color.reset}")
+                    print(f"{colors.green}\RED_HAWK is installed at ' {os.getcwd()} ' path\n{colors.reset}")
+                    ask_s = input(f"{colors.blue}[+] Do you want to start RED_HAWK? (y/n){colors.reset}")
                     if ask_s == "y" or ask_s == "Y" or ask_s == "yes" or ask_s == "YES":
                         os.system("cd RED_HAWK && php rhawk.php")
             else:
-                print(f"{color.green}[+] RED_HAWK is already Installed !!{color.reset}")
-                ask_s= input(f"{color.blue}[+] Do you want to start RED_HAWK? (y/n){color.reset}")
+                print(f"{colors.green}[+] RED_HAWK is already Installed !!{colors.reset}")
+                ask_s= input(f"{colors.blue}[+] Do you want to start RED_HAWK? (y/n){colors.reset}")
                 if ask_s == "y" or ask_s == "Y" or ask_s == "yes" or ask_s == "YES":
                     os.system("cd RED_HAWK && php rhawk.php")
 
@@ -206,20 +206,20 @@ def Th3inspector():
         ask = tool_options()
         if ask == "1":
             print(f"{colors.blue}[+] Download/usage")
-            print(f"{color.blue}Checking Th3inspector is installed or not......{color.reset}")
+            print(f"{colors.blue}Checking Th3inspector is installed or not......{colors.reset}")
             if not os.path.isdir("Th3inspector"):
-                print(f"{color.red}[-] Th3inspector is not installed{color.reset}")
-                check_y = input(f"{color.blue}[+] Do you want to install it?(y/n){color.reset}")
+                print(f"{colors.red}[-] Th3inspector is not installed{colors.reset}")
+                check_y = input(f"{colors.blue}[+] Do you want to install it?(y/n){colors.reset}")
                 if check_y.lower() == "y" or check_y.lower()=="YES":
-                    print(f"{color.green}[+] Installing .......{color.reset}")
+                    print(f"{colors.green}[+] Installing .......{colors.reset}")
                     os.system("git clone https://github.com/Moham3dRiahi/Th3inspector.git")
-                    print(f"{color.green}\Th3inspector is installed at ' {os.getcwd()} ' path\n{color.reset}")
-                    ask_s = input(f"{color.blue}[+] Do you want to start Th3inspector? (y/n){color.reset}")
+                    print(f"{colors.green}\Th3inspector is installed at ' {os.getcwd()} ' path\n{colors.reset}")
+                    ask_s = input(f"{colors.blue}[+] Do you want to start Th3inspector? (y/n){colors.reset}")
                     if ask_s == "y" or ask_s == "Y" or ask_s == "yes" or ask_s == "YES":
                         os.system("cd Th3inspector && perl Th3inspector.pl")
             else:
-                print(f"{color.green}[+] Th3inspector is already Installed !!{color.reset}")
-                ask_s= input(f"{color.blue}[+] Do you want to start Th3inspector? (y/n){color.reset}")
+                print(f"{colors.green}[+] Th3inspector is already Installed !!{colors.reset}")
+                ask_s= input(f"{colors.blue}[+] Do you want to start Th3inspector? (y/n){colors.reset}")
                 if ask_s == "y" or ask_s == "Y" or ask_s == "yes" or ask_s == "YES":
                     os.system("cd Th3inspector && perl Th3inspector.pl")
 
