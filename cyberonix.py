a2f23d3d0798848c980e855242e3c9b9d93a4f08
Mohita9138 <mohit66.mg@gmail.com>
2023-02-04 01:28:01 +0530

    hardik

--- cyberonix.py
+++ cyberonix.py
@@ -28,7 +28,7 @@ try:
                 print(use.decode())
                 print("\u001b[32m[+] Cyberonix is UPDATED To Latest Version")
                 try:
-                    subprocess.run(['cyberonix'],shell=True, check = True)
+                    subprocess.run('cyberonix',shell=True, check = True)
                 except Exception as err:
                     print(err)
                     time.sleep(5)
