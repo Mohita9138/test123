fab18a3429012c99934f870e770e119f64aa8c6e
root <root@192.168.1.9>
2023-02-03 14:53:11 +0530

    Mohit

--- main/tools/Wireless_Hacking.py
+++ main/tools/Wireless_Hacking.py
@@ -189,7 +189,15 @@ def Aircrack():
         banner.main()
         banner.attack("Aircrack-ng")
         # github = github_getting_text("https://github.com/aircrack-ng/aircrack-ng", 'p[dir="auto"]', 1)
-        github = github_getting_text("https://github.com/aircrack-ng/aircrack-ng", 'ul[dir="auto"]', 0)
+        github = github_getting_text('''Aircrack-ng is a complete suite of tools to assess WiFi network security.
+
+It focuses on different areas of WiFi security:
+
+Monitoring: Packet capture and export of data to text files for further processing by third party tools.
+Attacking: Replay attacks, deauthentication, fake access points and others via packet injection.
+Testing: Checking WiFi cards and driver capabilities (capture and injection).
+Cracking: WEP and WPA PSK (WPA 1 and 2).
+All tools are command line which allows for heavy scripting. A lot of GUIs have taken advantage of this feature. It works primarily on Linux but also Windows, macOS, FreeBSD, OpenBSD, NetBSD, as well as Solaris and even eComStation 2.''')
         banner.description(github)
         
         ask = tool_options()
